677960d746d9d349474eeecee6be4ec3d0e8a60f
Leonardo Garreto <leonardogarreto@gmail.com>
2021-11-02 15:16:05 -0300

    Added range but doesnt work

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ let container = document.querySelector('.container');
 let i = 0
 const allDivs = []
 
+let sizeRange = document.querySelector('.size')
+// console.log(sizeRange)
+sizeRange.addEventListener('input', (e) => {
+    console.log(e)
+})
 while (i <= 256) {
     let div = document.createElement('div')
     div.classList.add('divsGrid')
