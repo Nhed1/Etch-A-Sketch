4887e65fc08f8e98a57974966a9c202414b2dcb4
Leonardo Garreto <leonardogarreto@gmail.com>
2021-11-06 14:26:56 -0300

    tentando ainda dar um clear nas grids

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 let container = document.querySelector('.container');
 let i = 0
 const allDivs = []
-
+let clear = document.querySelector('.clear')
 let sizeRange = document.querySelector('.size')
 
 function createGrid(value) {
     let gridSize = value ** 2
-    console.log(gridSize)
+    // clearGrid(container)
+    let style = 'style',
+        gridStyle = `grid-template-columns: repeat(${value},1fr); grid-template-rows: repeat(${value},1fr);`
+
     console.log(value)
     while (i < gridSize) {
-
         let div = document.createElement('div')
         div.classList.add('divsGrid')
         container.appendChild(div)
-        container.setAttribute('style', `grid-template-columns: repeat(${value},1fr); grid-template-rows: repeat(${value},1fr);`)
+        container.setAttribute(style, gridStyle)
         allDivs.push(div)
         i++
+    }
+}
+
+function clearGrid(container) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild) // remove all childs
     }
 }
 
